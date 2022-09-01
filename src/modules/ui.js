27553fcf0e6dafaa78cb74977596c8f64f43c34c
Dark-WizZ <mailwithakram@gmail.com>
2022-09-01 10:07:02 +0530

    Add function to toggle C and F

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -5,32 +5,55 @@ export default class UI {
   static init(){
     this.domLoad();
     this.render();
+    this.domLoad();
     this.bindEvents();
   }
 
   static render(){
+
   }
 
   static domLoad(){
     this.locationIP = document.getElementById('locationIP');
-    this.temperature = document.querySelector('.temperature')
+    this.temperature = document.querySelector('.temperature');
+    this.state = document.querySelector('.state');
+    this.error = document.querySelector('.error')
   }
 
   static bindEvents(){
     this.locationIP.addEventListener('keypress', (e) => {
       this.locationIPEnter(e.key)
     });
+    this.temperature.addEventListener('click', this.temperatureClk.bind(this));
   }
 
-  static temperatureRender(){
-    this.temperature.textContent = `${Weather.tempereature}`;
-  }
-
-  static async locationIPEnter(e){
-    if (e == 'Enter'){
-      const loc = this.locationIP.value;
-      await Weather.fetchData(loc);
+  static temperatureClk(e){
+    if(Weather.units=='metric'){
+    Weather.units = 'imperial'
+    Weather.symbol = '°F'
+    }else{
+      Weather.units = 'metric'
+      Weather.symbol = '°C'
     }
-    this.temperatureRender();
+    this.temperatureRender()
+  }
+
+  static async temperatureRender(){
+    const stateName = locationIP.value
+    Weather.city = (stateName)? stateName: Weather.city;
+    await Weather.fetchData();
+    if(Weather.error){
+      this.error.style.display = 'block';
+      return;
+    }
+    this.error.style.display = 'none';
+    this.temperature.textContent = `${Weather.tempereature}`;
+    this.state.textContent = Weather.city.toUpperCase();
+  }
+
+  static locationIPEnter(e){
+    if (e == 'Enter'){
+      this.temperatureRender()
+    }
   }
 }
--- src/modules/weather_api.js
+++ src/modules/weather_api.js
@@ -3,18 +3,21 @@ export default class Weather{
   static api = '2423a53c7a0cc2851f352aa684e674f2';
   static units = 'metric'
   static tempereature;
+  static symbol = '°C';
+  static city;
+  static error=false;
 
-  static async fetchData(city){
+  static async fetchData(){
     try{
-      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${this.api}&units=${this.units}`);
+      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&APPID=${this.api}&units=${this.units}`);
       const data = await response.json();
-      console.log(data)
       if(data.cod==404){
-        console.log(data.message);
+        this.error = true;
         return;
       }
+      this.error = false;
       const temp = data.main.temp;
-      this.tempereature = temp+'°C';
+      this.tempereature = temp+this.symbol;
     }catch(err){
       console.log(err);
     }
